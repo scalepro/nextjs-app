b2f6727e15de69fbc91e8e2c6f4cdcd793b0b285
scalepro <scaleprobusiness@gmail.com>
2023-03-05 13:25:42 -0300

    Add Stepper

--- src/components/app/InstallThemeModal.tsx
+++ src/components/app/InstallThemeModal.tsx
@@ -4,9 +4,10 @@ import { useForm, Controller } from 'react-hook-form';
 import MaskedInput from 'react-text-mask';
 import { classNames } from '@/services/functions';
 import { BlockPicker } from 'react-color';
-import { HiChevronDown } from 'react-icons/hi';
+import { HiChevronDown, HiColorSwatch, HiNewspaper, HiFlag } from 'react-icons/hi';
 import toast, { Toaster } from 'react-hot-toast';
 import Toast from '@/components/app/Toast';
+import Stepper from '@/components/app/Stepper';
 import {
   defaultInput,
   errorInput,
@@ -23,6 +24,23 @@ export default function InstallThemeModal({ themeModalView, setThemeModalView })
   const secondaryColorPickerRef = useRef(null);
   const [headerPrimary, setHeaderPrimary] = useState(false);
   const [loadingSubmit, setLoadingSubmit] = useState(false);
+  const [actualStep, setActualStep] = useState(0);
+
+  const infoSteps = [
+    {
+      name: 'colors',
+      icon: HiColorSwatch
+    },
+    {
+      name: 'messages',
+      icon: HiNewspaper
+    },
+    {
+      name: 'finish',
+      icon: HiFlag
+    },
+  ];
+
   const colors = [
     "#f44336", "#1976d2",
     "#e91e63", "#689f38", 
@@ -99,23 +117,7 @@ export default function InstallThemeModal({ themeModalView, setThemeModalView })
               <div>
                 <div className="grid grid-cols-4 gap-4">
                   <div className="col-span-4">
-                    <ol className="flex items-center w-full">
-                      <li className="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-700">
-                        <span className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-600 shrink-0">
-                          <svg aria-hidden="true" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                        </span>
-                      </li>
-                      <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-600">
-                        <span className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-600 shrink-0">
-                          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 lg:w-6 lg:h-6 dark:text-gray-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                        </span>
-                      </li>
-                      <li className="flex items-center w-full">
-                        <span className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-600 shrink-0">
-                          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 lg:w-6 lg:h-6 dark:text-gray-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                        </span>
-                      </li>
-                    </ol>
+                    <Stepper infoSteps={infoSteps} actualStep={actualStep} />
                   </div>
                   <div className="col-span-4">
                     <label htmlFor="primary_color" className={defaultLabel}>
@@ -247,24 +249,34 @@ export default function InstallThemeModal({ themeModalView, setThemeModalView })
             </Modal.Body>
             <Modal.Footer>
               <div className="w-full flex justify-end gap-3">
-                <Button
-                  color="gray"
-                  onClick={() => {
-                    setThemeModalView(false);
-                    resetElements();
-                  }}
-                >
-                  Cancelar
-                </Button>
-                {!loadingSubmit ? (
-                  <Button type="submit">Salvar</Button>
-                ) : (
-                  <Button>
-                    <div className="mr-3">
-                      <Spinner size="sm" light={true} />
-                    </div>
-                    Salvando ...
+                { actualStep == 0 ? (
+                  <Button
+                    color="gray"
+                    onClick={() => {
+                      setThemeModalView(false);
+                      resetElements();
+                    }}
+                  >
+                    Cancelar
                   </Button>
+                ) : (
+                  <Button onClick={() => setActualStep(actualStep - 1) } type="submit">Voltar</Button>
+                )}
+                {actualStep < (infoSteps.length - 1) ? (
+                  <Button onClick={() => setActualStep(actualStep + 1) } type="submit">Avançar</Button>
+                ) : (
+                  <>
+                    {!loadingSubmit ? (
+                      <Button type="submit">Salvar</Button>
+                    ) : (
+                      <Button>
+                        <div className="mr-3">
+                          <Spinner size="sm" light={true} />
+                        </div>
+                        Salvando ...
+                      </Button>
+                    )}
+                  </>
                 )}
               </div>
             </Modal.Footer>
